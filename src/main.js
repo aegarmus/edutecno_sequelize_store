e82d07d8e1b85a0327395388f7ae84f19ab4fb39
aegarmus <aegarmus@gmail.com>
2024-12-16 19:56:25 -0300

    feat: initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT} 🫎`);
+})
